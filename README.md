e4f74abdeeb4f47cfbf814e281afa4239cfc92fd
fizz <fizz_long@wochacha.com>
2016-08-23 10:53:35 +0800

    complete the description

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # MultiProcess
-the php multi process library
+the php multi process library depend on the pcntl php extension(php多进程类, 基于php的pcntl扩展)
+
+## usage
+### install
+use composer
+```
+{
+    {
+        "fizzday/multiprocess": "dev-master"
+    }
+}
+```
+or
+```
+composer require fizzday/multiprocess
+```
+### using(english)
+1. extends the `MultiProcess` abstract class
+2. rewrite the `myAct()` method, do the real things you need
+3. finally act run() method make it works
+
+the code likes in file `myMulti.php`:
+```
+<?php
+use Fizzday/MultiProcess/MultiProcess;
+
+class MyMulti extends MultiProcess
+{
+    /**
+     * @param $j the process num(第几个进程)
+     * @param $i the circle num (第几次循环)
+     */
+    public function myAct($j, $i)
+    {
+        echo "this is the {$i} circle(循环) {$j} process(进程) \r\n";
+        // example: 
+        // suppose we have 100 rows data in total, we fetch 20 rows in on circle for 5 process
+        // yet , we need circles: 100 / 20 = 5
+        // we shell handle (20/5) rows in per process
+        // suppose the datas in a array {$data), current process will handle the data index in the area:
+        // from  {$i*20 + ($j)*(20/5)} to {$i*20 + ($j+1)*(20/5)}        
+    }
+}
+
+$multi = new MyMulti(100, 20, 5);
+$multi->run();
+```
+> suggestion: we'd better use the php-cli run this file like `php myMulti.php`
+
+### using(中文)
+1. 继承`MultiProcess`抽象类
+2. 重写`myAct()`方法, 执行具体操作
+3. 最后执行`run()`方法
+
+创建代码文件 `myMulti.php`:
+```
+<?php
+use Fizzday/MultiProcess/MultiProcess;
+
+class MyMulti extends MultiProcess
+{
+    /**
+     * @param $j 第几个进程
+     * @param $i 第几次循环
+     */
+    public function myAct($j, $i)
+    {
+        echo "这是第 {$i} 次循环, 第 {$j} 个进程 \r\n";
+        // 举例: 
+        // 假设我们总共有100条数据, 每一次获取20条, 分5个进程来处理
+        // 那么, 就需要(100/200)次循环来处理
+        // 每个进程处理的数据条数为(20/5)
+        // 假设我们取的数据在一个数组里, 那么当前进程所要操作的数据对应的编号区间为{$i*20 + ($j)*(20/5)}到{$i*20 + ($j+1)*(20/5)}  
+    }
+}
+
+$multi = new MyMulti(100, 20, 5);
+$multi->run();
+```
+
+> 建议: 最好使用命令行执行(`php myMulti.php`)
+
--- src/MultiProcess.php
+++ src/MultiProcess.php
@@ -25,31 +25,31 @@ abstract class MultiProcess
     /**
      * 初始化
      * MultiProcess constructor.
-     * @param int $pcntl_total
-     * @param int $pcntl_perTotalNum
-     * @param int $pcntl_num
+     * @param int $pcntl_total       all datas num (数据总量)
+     * @param int $pcntl_perTotalNum every circle total mum (每一次的数据总量)
+     * @param int $pcntl_num         total process (总进程数)
      */
     public function __construct($pcntl_total = 1000000, $pcntl_perTotalNum = 10000, $pcntl_num = 100)
     {
-        $this->pcntl_total = $pcntl_total;
+        $this->pcntl_total       = $pcntl_total;
         $this->pcntl_perTotalNum = $pcntl_perTotalNum;
-        $this->pcntl_num = $pcntl_num;
+        $this->pcntl_num         = $pcntl_num;
     }
 
     /**
      * 多进程任务开始调用方法
      */
-    public function pcntlAct()
+    public function run()
     {
         // 需要循环的次数
-        $pcntl_actNum = ceil($this->pcntl_total / $this->pcntl_perTotalNum) ;	// 需要发送的次数
+        $pcntl_actNum = ceil($this->pcntl_total / $this->pcntl_perTotalNum);    // 需要发送的次数
 
-        for ($i=0; $i < $pcntl_actNum; $i++) {
+        for ($i = 0; $i < $pcntl_actNum; $i++) {
 
             // 定义记录子进程的数组
             $pids = array();
 
-            for ($j=0; $j < $this->pcntl_num; $j++) {
+            for ($j = 0; $j < $this->pcntl_num; $j++) {
 
                 $pids[$j] = pcntl_fork();
 
@@ -60,7 +60,7 @@ abstract class MultiProcess
 
                     case 0:
                         // 开启子进程成功, 做相应操作
-                        $this->pcntlTrue($j, $i);
+                        $this->myAct($j, $i);
                         exit;
 
                     default:
@@ -71,7 +71,7 @@ abstract class MultiProcess
 
             // 等待子进程退出并回收
             foreach ($pids as $k => $pid) {
-                if($pid) {
+                if ($pid) {
                     pcntl_waitpid($pid, $status);
                 }
             }
@@ -84,5 +84,5 @@ abstract class MultiProcess
      * @param $i num 第几次循环
      * @param $j num 本次循环的第几个进程
      */
-    abstract public function pcntlTrue($j, $i);
+    abstract public function myAct($j, $i);
 }
