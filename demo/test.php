b60eec9bc80abdb21fb07e6e69c5525cb99eab95
fizz <fizz_long@wochacha.com>
2016-12-09 15:53:33 +0800

    new write

--- demo/test.php
+++ demo/test.php
@@ -1,105 +1,99 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/MultiProcess.php';
 use Fizzday\MultiProcess\MultiProcess;
 
-class MyMulti extends MultiProcess
-{
-    /**
-     * 集成后重写的具体操作
-     * @param $i num 第几次循环
-     * @param $j num 本次循环的第几个进程
-     */
-    public function myAct($j, $i)
-    {
-//        $b = 0;
-        for ($a = 0; $a < ($this->pcntl_perTotalNum / $this->pcntl_num); $a++) {
-//            $this->b ++;
-            $requestUrl = 'http://www.test.com';
-            $post_data  = ['phone' => '132123' . mt_rand(10000, 99999)];
-            $aa         = explode("\r\n", curl_post($requestUrl, $post_data));
-//        echo $i.' -- '.$aa[0]."\r\n";
-            $bb = $j*$i+$a;
-            echo "{$bb} -- 第 $i 次({$post_data['phone']})执行, 第 $a 次操作, 第 $j 个进程 -- " . $aa[0] . "\r\n";
-        }
+//echo time();
+// 设置将要开启的进程数
+$process = 100;
+$res     = MultiProcess::setProcess($process)->run(function ($p) use ($process) {
+    // 数据总量
+    $total = 10000;
+    // 每个进程对应要处理的数据总量
+    $perProcessNum = $total / $process;
 
+    $start = $p * $perProcessNum;     // 开始处理数据的索引(包含开头)
+    $end   = $start + $perProcessNum;   // 结束处理数据的索引(不包含结尾)
 
+    // 模拟将要处理的数据
+    $data = array();
+    for ($a = 0; $a < $total; $a++) {
+        $data[] = $a . 'aa';
     }
 
+    // 开始处理数据
+    $data_return = '';
+    for ($i = $start; $i < $end; $i++) {
+//        $data_return .= '进程编号:' . $p . '-处理数据:' . $data[$i] . PHP_EOL;
+
+        $mobile = 13;
+        $mobile .= mt_rand(10000, 99999).mt_rand(1000, 9999);
+
+        $res = sendsms($mobile);
+
+        $data_return .= $res.'-进程编号:' . $p . '-处理数据:' . $mobile . PHP_EOL;
+    }
+
+    $data_return .= PHP_EOL;
+
+    $file = './log.txt';
+    file_set($file, $data_return);
+
+    echo $data_return;
+
+//    return $data_return;
+});
+
+
+//print_r($res);
+
+//echo time();
+function file_set($file, $data, $mode = 'a')
+{
+    if (!$file || !$data) return false;
+
+//    $dir = dirname($file);
+//
+//    if (!is_dir($dir)) mkdir($dir, 0777, true);
+
+    $fp = fopen($file, $mode);
+    fwrite($fp, $data);
+    fclose($fp);
+
+    chmod($file, 0777);
+
+    return true;
 }
 
-// 开始使用
-$myPcntl = new MyMulti(100000, 10000, 100);
-
-$myPcntl->run();
-
-//$proxy = [
-//    'ip'    => '112.65.219.'.mt_rand(1, 255),
-//    'port'  => '80'
-//];
-
-//$ch = curl_init();
-//$timeout = 5;
-//curl_setopt($ch, CURLOPT_URL, $requestUrl);
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-//curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_BASIC); //代理认证模式
-//curl_setopt($ch, CURLOPT_PROXY, "112.65.219.72"); //代理服务器地址
-//curl_setopt($ch, CURLOPT_PROXYPORT, 80); //代理服务器端口
-////curl_setopt($ch, CURLOPT_PROXYUSERPWD, ":"); //http代理认证帐号，username:password的格式
-//curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //使用http代理模式
-//// 添加post数据到请求中
-//curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-//$file_contents = curl_exec($ch);
-//curl_close($ch);
-//echo $file_contents;
-
-
-//for ($i=0; $i<20000; $i++) {
-//    $aa = explode("\r\n", curl_post($requestUrl, $post_data));
-//    echo $i.' -- '.$aa[0]."\r\n";
-//}
-
-
-function curl_post($url, $post_data, $proxy = [])
+//https://www.jppatt.com/open/SendForRegister
+//country:86
+//phone:13212341123
+function sendsms($mobile)
 {
-    //初始化一个 cURL 对象
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $url  = 'https://www.jppatt.com/open/SendForRegister';
+    $data = array(
+        "country" => "86",
+        "phone"   => $mobile
+    );
 
-//    if (!empty($proxy)) {
-//        curl_setopt($ch,CURLOPT_PROXY,$proxy['ip']);
-//        curl_setopt($ch,CURLOPT_PROXYPORT,$proxy['port']);
-//    }
+    $res = send_post($url, $data);
 
-    // 设置请求为post类型
-    curl_setopt($ch, CURLOPT_POST, 1);
-    // 添加post数据到请求中
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-
-    // 执行post请求，获得回复
-    $response = curl_exec($ch);
-    curl_close($ch);
-
-    return $response;
+    return $res;
 }
 
 
-function send_post($url, $post_data)
-{
-
+function send_post($url, $post_data) {
     $postdata = http_build_query($post_data);
-    $options  = array(
+    $options = array(
         'http' => array(
-            'method'  => 'POST',
-            'header'  => 'Content-type:application/x-www-form-urlencoded',
+            'method' => 'POST',
+            'header' => 'Content-type:application/x-www-form-urlencoded',
             'content' => $postdata,
             'timeout' => 15 * 60 // 超时时间（单位:s）
         )
     );
-    $context  = stream_context_create($options);
-    $result   = file_get_contents($url, false, $context);
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
 
     return $result;
 }
--- src/MultiProcess.php
+++ src/MultiProcess.php
@@ -2,87 +2,62 @@
 
 namespace Fizzday\MultiProcess;
 
-abstract class MultiProcess
+class MultiProcess
 {
-    /**
-     * all datas num(数据总数量)
-     * @var int
-     */
-    public $pcntl_total;
-
-    /**
-     * every circle total mum (每一次的数据总量)
-     * @var int
-     */
-    public $pcntl_perTotalNum;
-
     /**
      * total process (总进程数)
      * @var int
      */
-    public $pcntl_num;
+    public static $process=10;
 
     /**
-     * 初始化
-     * MultiProcess constructor.
-     * @param int $pcntl_total       all datas num (数据总量)
-     * @param int $pcntl_perTotalNum every circle total mum (每一次的数据总量)
-     * @param int $pcntl_num         total process (总进程数)
+     * 设置进程数
+     * @param int $process
+     * @return MultiProcess
      */
-    public function __construct($pcntl_total = 1000000, $pcntl_perTotalNum = 10000, $pcntl_num = 100)
+    public static function setProcess($process=10)
     {
-        $this->pcntl_total       = $pcntl_total;
-        $this->pcntl_perTotalNum = $pcntl_perTotalNum;
-        $this->pcntl_num         = $pcntl_num;
+        self::$process = $process;
+        return new self();
     }
 
     /**
      * 多进程任务开始调用方法
+     * @param $act
      */
-    public function run()
+    public static function run($act)
     {
-        // 需要循环的次数
-        $pcntl_actNum = ceil($this->pcntl_total / $this->pcntl_perTotalNum);    // 需要发送的次数
+        if (!function_exists('pcntl_fork')) return 'pcntl extension is not exists, please install it frstly';
+        // 定义记录子进程的数组
+        $pids = array();
 
-        for ($i = 0; $i < $pcntl_actNum; $i++) {
+        for ($i = 0; $i < self::$process; $i++) {
 
-            // 定义记录子进程的数组
-            $pids = array();
+            $pids[$i] = pcntl_fork();
 
-            for ($j = 0; $j < $this->pcntl_num; $j++) {
+            switch ($pids[$i]) {
+                case -1:
+//                    echo "开启子进程失败 : {$i} \r\n";
+                    echo "fork process failed : {$i} \r\n";
+                    exit;
 
-                $pids[$j] = pcntl_fork();
+                case 0:
+                    // 开启子进程成功, 做相应操作
+                    $act($i);
+                    exit;
 
-                switch ($pids[$j]) {
-                    case -1:
-                        echo "开启子进程失败 : {$j} \r\n";
-                        exit;
-
-                    case 0:
-                        // 开启子进程成功, 做相应操作
-                        $this->myAct($j, $i);
-                        exit;
-
-                    default:
-                        break;
-                }
-
-            }
-
-            // 等待子进程退出并回收
-            foreach ($pids as $k => $pid) {
-                if ($pid) {
-                    pcntl_waitpid($pid, $status);
-                }
+                default:
+                    break;
             }
 
         }
-    }
 
-    /**
-     * 集成后重写的具体操作
-     * @param $j the process num(第几个进程)
-     * @param $i the circle num (第几次循环)
-     */
-    abstract public function myAct($j, $i);
+        // 等待子进程退出并回收
+        foreach ($pids as $k => $pid) {
+            if ($pid) {
+                pcntl_waitpid($pid, $status);
+            }
+        }
+    }
 }
+
